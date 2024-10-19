43dadf8a3f1b495751c702b97d45f11e7d79e3eb
deveshrai1905 <rai.devesh1@gmail.com>
2024-10-19 15:17:25 -0700

    massive changes to codebase - moved all images in-house

--- models.py
+++ models.py
@@ -1,14 +1,30 @@
-from extensions import db  # Import db from extensions
+from extensions import db
 from datetime import datetime
+
+# Association table for the many-to-many relationship between Pokemon and Type
+pokemon_types = db.Table('pokemon_types',
+    db.Column('pokemon_id', db.Integer, db.ForeignKey('pokemon.id'), primary_key=True),
+    db.Column('type_id', db.Integer, db.ForeignKey('type.id'), primary_key=True)
+)
 
 class Pokemon(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
+    generation = db.Column(db.Integer)  # New field for generation
     elo_rating = db.Column(db.Float, default=1000.0)
-    image_url = db.Column(db.String(200))
+    # image_url = db.Column(db.String(200))  # Original image URL (optional)
+    sprite_filename = db.Column(db.String(200))  # Filename for local sprite
+    types = db.relationship('Type', secondary=pokemon_types, backref=db.backref('pokemon', lazy='dynamic'))
 
     def __repr__(self):
         return f"<Pokemon {self.name}>"
+
+class Type(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+
+    def __repr__(self):
+        return f"<Type {self.name}>"
 
 class Match(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -20,4 +36,4 @@ class Match(db.Model):
     loser = db.relationship('Pokemon', foreign_keys=[loser_id], backref='losses')
 
     def __repr__(self):
-        return f"<Match {self.winner_id} defeated {self.loser_id}>"
+        return f"<Match {self.winner.name} defeated {self.loser.name}>"
